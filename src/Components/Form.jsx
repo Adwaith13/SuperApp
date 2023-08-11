8d8e83b7b62f70b2ba4446188e72b061cc15cd1e
Adwaith Nair <adwaith1310@gmail.com>
2023-08-11 22:48:30 +0530

    boilerplate react setup

---
+++ src/Components/Form.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+export default function Form() {
+    useEffect(()=>{
+        document.title='Super App'
+    })
+  return (
+    <div>
+        
+    </div>
+  )
+}
